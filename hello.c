6afa89d9b57568bfed57ab21f8886431d61e6e50
Abraham Varricatt <abraham.varricatt@googlemail.com>
2013-12-19 10:11:20 +0530

    Adding a sample source file + doxy config

---
+++ hello.c
@@ -0,0 +1,25 @@
+/**
+ * @brief  Brief description: no more than one line
+ *
+ * This is the longer description. There must be a blank line separating
+ * the brief comment from the longer description.
+ * Ripping this file from http://www.openlogic.com/wazi/bid/188121/Doxygen-Magically-Turns-Source-Code-into-Documentation
+ */
+
+/**
+ * @file   hello.c
+ * @author Abraham
+ * @date   19/Dec/2013
+.*/
+
+#include <stdio.h>
+
+/**
+* This is a demo comment, which can ramble on for as long
+* as you like
+*/
+
+void main()
+{
+    printf("Hello World");
+}
